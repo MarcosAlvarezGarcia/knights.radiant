6580e9f582bed927cd097cf8b9948a6ccc928f4a
MarcosAlvarezGarcia <marcos.alv.garcia@gmail.com>
2024-03-09 12:51:15 +0100

    Probando que funciona git

--- src/main/java/marcos/knights/radiant/Application.java
+++ src/main/java/marcos/knights/radiant/Application.java
@@ -7,7 +7,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println("Hasieratzen/Starteando...");
 		SpringApplication.run(Application.class, args);
-	}
+		System.out.println("*******************************************************************************************************************************************************************************************");
+		System.out.println("*                                                                                                                                                      			                          *");
+		System.out.println("*                                                                                                                                                      			       			          *");
+		System.out.println("*                                                                                                                                                                           		      *");
+		System.out.println("*                                                                                    WELCOME                                                                             		          *");
+		System.out.println("*                                                                                    TO THE                                                                              		          *");
+		System.out.println("*                                                                                KNIGHTS RADIANT!                                                                                         *");
+		System.out.println("*                                                                                                                                                      			            		      *");
+		System.out.println("*                                                                                                                                                                          			      *");
+		System.out.println("*******************************************************************************************************************************************************************************************");
 
+		System.out.println("Abra Postman en http://localhost:8080");
+	}
 }
